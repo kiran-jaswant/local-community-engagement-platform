0bae008e99cf1d0d1aa2503ebde772b8ad59d6eb
kiran-jaswant <kiranjaswant08@gmail.com>
2024-08-23 12:18:53 +0530

    bg img added

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Home.css'
+import bgimg from './bg.jpg'
+
+
 
 
 function Home() {
   return (
     <div className=''>
-      <h1 className=''>Home</h1>
+        <img src={bgimg} className='w-screen'></img>
     </div>
   )
 }
 
 export default Home
+
